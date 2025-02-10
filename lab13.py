590c1340312f430d35249693ee19f53d0bc58ed8
Michael Korniichuk <misha17092006@gmail.com>
2025-02-10 23:43:46 +0200

    Оновив lab13.py: з використанням sum(), for i try...except

--- lab13.py
+++ lab13.py
@@ -1,9 +1,17 @@
 def sum_positive_numbers(*numbers):
-    results = [sum(range(num + 1)) if num > 0 else 0 for num in numbers]
+    results = []
+    for num in numbers:
+        try:
+            if num > 0:
+                results.append(sum(range(num + 1)))
+            else:
+                results.append(0)
+        except TypeError:
+            results.append("Error: Invalid input")
     return results
 
 
 # Приклад виклику функції
 if __name__ == "__main__":
-    test_numbers = [3, 5, -2, 0, 7]
+    test_numbers = [3, 5, -2, 0, 7, "text"]
     print(sum_positive_numbers(*test_numbers))
